02040312fcc59dddd5cb1be79be730b1f8c2bf47
thathsaranipun <nipunariyarathna@gmail.com>
2021-12-29 16:04:48 +0530

    Added

---
+++ favourite/favButton.js
@@ -0,0 +1,26 @@
+console.log("start saving");
+var favouriteItems = [];
+var checkButtonClick1 = false;
+$('#fav1').hide();
+
+$("#restaurant01").on('click', function (event) {
+    event.preventDefault();
+    if (checkButtonClick1 == false) {
+        $('#fav1').show();
+        $('#fav').hide();
+        checkButtonClick1 = true;
+        favouriteItems.push(product1)
+        localStorage.setItem('favouriteList', JSON.stringify(favouriteItems));
+    } else {
+        $('#fav1').hide();
+        $('#fav').show();
+        checkButtonClick1 = false;
+        for (i = 0; i < favouriteItems.length; i++) {
+            if (favouriteItems[i].productId == 1) {
+                var updatedFavList = favouriteItems.filter(item => item.productId != 1);
+                favouriteItems = updatedFavList;
+                localStorage.setItem('favouriteList', JSON.stringify(favouriteItems));
+            }
+        }
+    }
+});
